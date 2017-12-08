1b3edd60fa6362b08dc79cbc705f230fc8fcaeaa
Elizabeth H Burgess <heb264@gmail.com>
2017-12-08 09:10:38 +0800

    added service

--- app/weather-widget/service/weather.service.ts
+++ app/weather-widget/service/weather.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
+import {Jsonp} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
+//constant import
 import { FORECAST_KEY, FORECAST_ROOT} from '../constants/constants';
 
 @Injectable()
 export class WeatherService{
+    
+    constructor(private jsonp: Jsonp){}
     getCurrentLocation(): [number,number]{
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(pos => {
-                console.log("Position: ", pos.coords.latitude,  ":" ,pos.coords.longitude); //TODO REMOVE
+                console.log("Position: ", pos.coords.latitude,  "|" ,pos.coords.longitude); //TODO REMOVE
                 return[pos.coords.latitude, pos.coords.longitude]
             },
-        error => console.error("Unable to get the postion -", error))
+        error => console.error("Unable to get the postion -", error));
         }else{
             console.error{"Geolocation is not available"};
             return [0,0]
 
         }
+    }
+
+    getCurrentWeather(lat: number, long: number):Observable<any>{
+        const url = FORECAST_ROOT + FORECAST_KEY +"/"+ lat + ","+long;
+        const queryParams = "?callback=JSONP_CALLBACK";
+
+        return this.jsonp.get(url + queryParams)
+        .map(data => data.json())
+        .catch(err => {
+            console.error("unable to get weather data - ", err);
+            return Observable.throw(err.json());
+        });
     }
  }
 
